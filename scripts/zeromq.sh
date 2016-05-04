5e0c9fe20459b25dc5bf29b9b2e26d83cbf49de8
Felix Imobersteg <felix@whatwedo.ch>
2016-05-04 23:17:57 +0200

    fix(zeromq): removed php module installation

--- scripts/zeromq.sh
+++ scripts/zeromq.sh
@@ -4,8 +4,6 @@ echo ">>> Installing ØMQ"
 
 add-apt-repository -y ppa:chris-lea/zeromq 
 apt-get update -qq
-apt-get install -qq libtool autoconf automake uuid uuid-dev uuid-runtime build-essential php5-dev pkg-config libzmq3-dbg libzmq3-dev libzmq3
-
-echo "" | pecl install zmq-beta > /dev/null
+apt-get install -qq libtool autoconf automake uuid uuid-dev uuid-runtime pkg-config libzmq3-dbg libzmq3-dev libzmq3
 
 
