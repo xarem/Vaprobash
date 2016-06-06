53db1f3be64ec114c983b60501426150e9057a47
Ueli Banholzer <ueli@whatwedo.ch>
2016-06-06 18:36:40 +0200

    chore(wkhtmltopdf): fix typo

--- scripts/wkhtmltopdf.sh
+++ scripts/wkhtmltopdf.sh
@@ -2,7 +2,7 @@
 
 echo ">>> Installing wkhtmltopdf"
 
-# Installwkhtmltopdf
+# Install wkhtmltopdf
 apt-get install -qq wkhtmltopdf xvfb xfonts-75dpi
 wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
 dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
